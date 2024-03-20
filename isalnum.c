6ef4009cd4197a3a6389126cffecaa929c6d8819
Carmen de Aguilar <153908439+cade-agu@users.noreply.github.com>
2024-03-20 11:45:29 +0100

    Create isalnum.c

---
+++ isalnum.c
@@ -0,0 +1,14 @@
+#include "libft.h"
+
+int	ft_isalnum(int c)
+{
+	if ((c >= '0' && c <= '9') || ((c >= 'a' && c <= 'z')
+			|| (c >= 'A' && c <= 'Z')))
+		return (1);
+	return (0);
+}
+/* int main(void)
+{
+	printf("%d", ft_isalnum ('T'));
+	return (0);
+}*/
