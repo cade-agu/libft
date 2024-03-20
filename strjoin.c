8c4c16dd9321f70f276e0a202b20a37717442865
Carmen de Aguilar <153908439+cade-agu@users.noreply.github.com>
2024-03-20 12:11:34 +0100

    Create strjoin.c

---
+++ strjoin.c
@@ -0,0 +1,26 @@
+
+#include "libft.h"
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	int		i;
+	int		iptr;
+	char	*ptr;
+	int		len;
+
+	i = 0;
+	iptr = 0;
+	if (!s1)
+		return (NULL);
+	len = (ft_strlen((char *)s1) + ft_strlen((char *)s2) + 1);
+	ptr = malloc(sizeof(char) + len);
+	if (!s1 || !s2 || !ptr)
+		return (NULL);
+	while (s1[i])
+		ptr[iptr++] = s1[i++];
+	i = 0;
+	while (s2[i])
+		ptr[iptr++] = s2[i++];
+	ptr[iptr] = '\0';
+	return (ptr);
+}
