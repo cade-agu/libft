ddef01fe050f37720fe39ef552998710cacd86ac
Carmen de Aguilar <153908439+cade-agu@users.noreply.github.com>
2024-03-20 12:40:28 +0100

    Create strtrim.c

---
+++ strtrim.c
@@ -0,0 +1,16 @@
+
+#include "libft.h"
+
+char	*ft_strtrim(char const *s1, char const *set)
+{
+	size_t	i;
+
+	if (!s1 || !set)
+		return (NULL);
+	while (ft_strchr(set, *s1) && *s1 != '\0')
+		s1++;
+	i = ft_strlen((char *)s1);
+	while (ft_strchr(set, s1[i]) && i != '\0')
+		i--;
+	return (ft_substr((char *)s1, 0, i + 1));
+}
